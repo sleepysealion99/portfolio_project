1cfd5c8a615c3d9ff5d0917b3f5116d7aafd1970
sleepysealion99 <yiran_shao+cs1300@brown.edu>
2022-12-12 22:24:12 -0500

    10

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import './App.css';
 import Homepage from './pages/home/homepage';
 import Main from './components/Main';
 import React, { Component } from "react";
-import { Route, HashRouter, Routes, BrowserRouter } from "react-router-dom";
+import { Route, HashRouter, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import Projects from "./pages/projects/projects";
 import Project1 from "./pages/project1/project1";
 import Project2 from "./pages/project2/project2";
 import Project3 from "./pages/project3/project3";
 import Project4 from "./pages/project4/project4";
-
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <div>
           <BrowserRouter>
+            <Navbar/>
             <Routes>
               <Route
                 exact path="/" element={<Homepage/>}
@@ -41,7 +43,9 @@ function App() {
               <Route
                 path="/projects/iterative_design" element={<Project4/>}
               />
+              <Route path="*" element={<Navigate to="/"/>} />
             </Routes>
+            <Footer/>
           </BrowserRouter>
         </div>
     </div>
